4fff59f06e21996f85b65ec52f1786d1df938053
lindsey3291 <lindseytrinh0919@gmail.com>
2026-09-06 22:54:15 -0400

    Add boxplot script

---
+++ src/boxplot.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import fetch_california_housing
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load California Housing dataset
+housing = fetch_california_housing(as_frame=True)
+df = housing.frame
+
+# Boxplot of Median Income
+plt.figure(figsize=(6, 4))
+plt.boxplot(df['MedInc'])
+plt.title('Boxplot of Median Income')
+plt.ylabel('MedInc')
+plt.savefig('boxplot.png')
+plt.show()
